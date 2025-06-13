d60d3d7a2ce4613ce79aafecd988a122362d868f
shimarda <ryo1031shima@gmail.com>
2025-06-13 23:25:45 +0900

    mult thread

---
+++ hello/target/release/deps/hello-51646203461923fd.d
@@ -0,0 +1,5 @@
+/home/smd/study/rust_god_practice/hello/target/release/deps/hello-51646203461923fd: src/main.rs
+
+/home/smd/study/rust_god_practice/hello/target/release/deps/hello-51646203461923fd.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/release/hello.d
@@ -0,0 +1,1 @@
+/home/smd/study/rust_god_practice/hello/target/release/hello: /home/smd/study/rust_god_practice/hello/src/main.rs
